1c93d48948557abbbb78d67deece22b728d14596
Elizaveta Kirichok <Tyuiu.KirichokEV@outlook.com>
2024-10-18 22:11:58 +0500

    Task1

---
+++ Tyuiu.KirichokEV.Sprint1.Task1.V22.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+using Tyuiu.KirichokEV.Sprint1.Task1.V22.Lib;
+
+namespace Tyuiu.KirichokEV.Sprint1.Task1.V22.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 1.0;
+            double y = 2.0;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(3 , res);
+        }
+    }
+}
---
+++ Tyuiu.KirichokEV.Sprint1.Task1.V22/Program.cs
@@ -0,0 +1,45 @@
+﻿using Tyuiu.KirichokEV.Sprint1.Task1.V22.Lib;
+
+namespace Tyuiu.KirichokEV.Sprint1.Task1.V22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнила: Киричлк Е. В.| РППб-24-1";
+            //Длина строки 75 символов 
+            Console.WriteLine("*************************************************************************************");
+            Console.WriteLine("* Спринт #1                                                                         *");
+            Console.WriteLine("* Тема: Базовые навыки работы в С#                                                  *");
+            Console.WriteLine("* Задание #1                                                                        *");
+            Console.WriteLine("* Вариант #22                                                                       *");
+            Console.WriteLine("* Выполнила: Киричок Елиавета Викторовна | РППб-24-1                                *");
+            Console.WriteLine("*************************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                          *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные,           *");
+            Console.WriteLine("* вычисляет результат по фоормуле ( 5 + x) / (y * x) и печатает его на экране.      *");
+            Console.WriteLine("*                                                                                   *");
+            Console.WriteLine("*************************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                  *");
+            Console.WriteLine("*************************************************************************************");
+
+            double x, y;
+
+            Console.WriteLine("Введите значение X:");
+            x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите значение Y:");
+            y = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("*************************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                        *");
+            Console.WriteLine("*************************************************************************************");
+
+            Console.WriteLine(ds.Calculate(x, y));
+
+            Console.ReadLine();
+        }
+    }
+}
