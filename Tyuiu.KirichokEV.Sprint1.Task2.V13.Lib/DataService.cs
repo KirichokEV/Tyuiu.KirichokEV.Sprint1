f11b085760007cc4040d9e294d8425fe31bbf023
Elizaveta Kirichok <Tyuiu.KirichokEV@outlook.com>
2024-10-20 12:49:18 +0500

    Task2

--- Tyuiu.KirichokEV.Sprint1.Task2.V13.Lib/DataService.cs
+++ Tyuiu.KirichokEV.Sprint1.Task2.V13.Lib/DataService.cs
@@ -6,7 +6,7 @@ namespace Tyuiu.KirichokEV.Sprint1.Task2.V13.Lib
     {
         public double ConvertMilesToKm(int value)
         {
-            return value * 1.609; 
+            return value * 1.60934; 
         }
     }
 }
